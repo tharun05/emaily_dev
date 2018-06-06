b8131c07fa34d18a2d49ef6924a7d9591403d5a7
tharun05 <tharundillikar@gmail.com>
2018-06-06 09:16:18 +0530

    heroku test

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
-
+const passport = require('passport')
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
 
-app.get('/',function(req,res){
-    res.send({hi:'there haha'})
-})
+// passport.use(new GoogleStrategy({
+//     clientID: GOOGLE_CLIENT_ID,
+//     clientSecret: GOOGLE_CLIENT_SECRET,
+// }))
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT)
